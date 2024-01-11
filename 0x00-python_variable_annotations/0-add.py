ba962a23a205fcce3b77f70d88b15a64c9f6581a
Heba Omar <jtvonline@yahoo.com>
2024-01-11 21:13:06 +0300

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-
-"""
-Variable annotacion
-"""
+'''
+    Basic annotations for variables.
+'''
 
 
 def add(a: float, b: float) -> float:
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
