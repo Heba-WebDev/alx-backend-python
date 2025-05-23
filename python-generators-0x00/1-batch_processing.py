19d8fcf0c3f5836bef0e11a0ba190ec4c02be61e
Heba-Webdev23 <jtvonline@yahoo.com>
2025-05-23 14:25:36 +0300

    feat: processing large data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,33 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def stream_users_in_batches(batch_size):
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database="ALX_prodev"
+    )
+
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    conn.close()
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
