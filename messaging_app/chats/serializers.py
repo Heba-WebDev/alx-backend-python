217ef839e617a7e7f6892e7c16dfed6cb4ac7a59
heba-webdev <jtvonline@yahoo.com>
2025-05-27 21:34:26 +0300

    serializering

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -10,10 +10,19 @@ class UserSerializer(serializers.ModelSerializer):
     def get_full_name(self, obj):
         return f"{obj.first_name} {obj.last_name}"
 
+
 class MessageSerializer(serializers.ModelSerializer):
+    sender_email = serializers.CharField(source='sender.email', read_only=True) 
+
     class Meta:
         model = Message
-        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        fields = ['id', 'sender', 'sender_email', 'conversation', 'content', 'timestamp']
+
+    def validate_content(self, value):  # ← ValidationError example
+        if not value.strip():
+            raise serializers.ValidationError("Message content cannot be empty.")
+        return value
+
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
