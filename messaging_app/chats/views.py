0cdabb0d32a8c42e42a7a8b7f4e05b237365a21f
Heba-Webdev23 <jtvonline@yahoo.com>
2025-06-08 13:22:23 +0300

    feat: permissions

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -5,13 +5,14 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from django.contrib.auth import get_user_model
+from .permissions import IsParticipantOfConversation
 
 User = get_user_model()
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
         return self.queryset.filter(participants=self.request.user)
@@ -39,7 +40,7 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
         conversation_id = self.request.query_params.get('conversation')
